60c9c4e043165d5e6e9e482ca819c3a0d1a2a4df
src_resources <src.res.211@gmail.com>
2024-04-20 11:31:01 +0800

    Improve the GitHub Actions workflow
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Installing necessary apt packages..."
-sudo apt update
-sudo apt install build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
-sudo apt install texlive texlive-latex-extra
+sudo apt-get update
+sudo apt-get install build-essential python3-dev libcairo2-dev libpango1.0-dev ffmpeg
+sudo apt-get install texlive texlive-latex-extra
+sudo apt-get install texstudio texlive-lang-chinese
 
 echo "Installing necessary LaTeX macro packages..."
 sudo tlmgr install collection-basic amsmath babel-english cbfonts-fd cm-super ctex doublestroke\
