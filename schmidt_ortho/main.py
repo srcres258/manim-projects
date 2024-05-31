6f1e68711477f21dce1c51f5c9a90ba63e50ad7e
src_resources <src.res.211@gmail.com>
2024-05-31 22:58:20 +0800

    Update schmidt_ortho

--- schmidt_ortho/main.py
+++ schmidt_ortho/main.py
@@ -1,7 +1,6 @@
 from manim import *
 
 import numpy as np
-import random
 
 
 class SchmidtOrtho2D(Scene):
@@ -17,8 +16,8 @@ class SchmidtOrtho2D(Scene):
             always_update_mobjects,
             random_seed,
             skip_animations)
-        self.vec_a_1 = np.array([random.randint(-3, 3), random.randint(-3, 3)], dtype=np.float32)
-        self.vec_a_2 = np.array([random.randint(-3, 3), random.randint(-3, 3)], dtype=np.float32)
+        self.vec_a_1 = np.array([-2, -4], dtype=np.float32)
+        self.vec_a_2 = np.array([3, -1], dtype=np.float32)
 
         print("Vector a1: ", self.vec_a_1)
         print("Vector a2: ", self.vec_a_2)
@@ -171,6 +170,18 @@ class SchmidtOrtho2D(Scene):
         )
 
 
+def arrow_of(
+        end,
+        start=np.array([0, 0, 0], dtype=np.float32),
+        color=RED
+) -> Arrow3D:
+    return Arrow3D(
+        start=start,
+        end=end,
+        color=color
+    )
+
+
 class SchmidtOrtho3D(ThreeDScene):
 
     def __init__(self,
@@ -183,15 +194,80 @@ class SchmidtOrtho3D(ThreeDScene):
             ambient_camera_rotation,
             default_angled_camera_orientation_kwargs,
             **kwargs)
+        self.axes = None
+        self.vec_a_1 = np.array([1, 1, 1], dtype=np.float32)
+        self.vec_a_2 = np.array([2, -2, 1], dtype=np.float32)
+        self.vec_a_3 = np.array([-1, -1, 1], dtype=np.float32)
+
+        print("Vector a1: ", self.vec_a_1)
+        print("Vector a2: ", self.vec_a_2)
+        print("Vector a3: ", self.vec_a_3)
 
     def construct(self):
-        axes = ThreeDAxes()
+        self.axes = ThreeDAxes()
         self.set_camera_orientation(phi=75 * DEGREES, theta=0)
-        self.play(Create(axes, run_time=2))
+        self.play(Create(self.axes, run_time=2))
         self.wait(1)
 
         self.begin_ambient_camera_rotation(rate=0.05)
-        self.wait(5)
 
-        self.stop_ambient_camera_rotation()
+        vec_a_1_arrow = arrow_of(self.vec_a_1)
+        vec_a_2_arrow = arrow_of(self.vec_a_2)
+        vec_a_3_arrow = arrow_of(self.vec_a_3)
+        vec_a_1_tex = MathTex(r"\vec a_1 =", "({}, {}, {})".format(
+            self.vec_a_1[0], self.vec_a_1[1], self.vec_a_1[2]), color=RED)
+        vec_a_2_tex = MathTex(r"\vec a_2 =", "({}, {}, {})".format(
+            self.vec_a_2[0], self.vec_a_2[1], self.vec_a_2[2]), color=RED)
+        vec_a_3_tex = MathTex(r"\vec a_3 =", "({}, {}, {})".format(
+            self.vec_a_3[0], self.vec_a_3[1], self.vec_a_3[2]), color=RED)
+        self.add_fixed_in_frame_mobjects(vec_a_1_tex, vec_a_2_tex, vec_a_3_tex)
+        vec_a_1_tex.to_corner(UP + LEFT)
+        vec_a_2_tex.next_to(vec_a_1_tex, DOWN)
+        vec_a_3_tex.next_to(vec_a_2_tex, DOWN)
+        self.play(
+            Create(vec_a_1_arrow, run_time=2),
+            Create(vec_a_2_arrow, run_time=2),
+            Create(vec_a_3_arrow, run_time=2),
+            Write(vec_a_1_tex, run_time=2),
+            Write(vec_a_2_tex, run_time=2),
+            Write(vec_a_3_tex, run_time=2)
+        )
+        self.wait(1)
+
+        vec_b_1 = self.vec_a_1.copy()
+        vec_b_1_arrow = vec_a_1_arrow.copy().set_color(YELLOW)
+        vec_b_1_tex = MathTex(r"\vec b_1 = \vec a_1", color=YELLOW)
+        self.add_fixed_in_frame_mobjects(vec_b_1_tex)
+        vec_b_1_tex.next_to(vec_a_3_tex, DOWN)
+        self.play(
+            Create(vec_b_1_arrow, run_time=2),
+            Write(vec_b_1_tex, run_time=2)
+        )
+        self.wait(1)
+
+        vec_b_2_tex = MathTex(r"\vec b_2 = \vec a_2 - \frac{(\vec a_2, \vec b_1)}{(\vec b_1, \vec b_1)}\vec b_1",
+                              color=YELLOW)
+        self.add_fixed_in_frame_mobjects(vec_b_2_tex)
+        vec_b_2_tex.next_to(vec_b_1_tex, DOWN)
+        vec_b_2_tex.align_on_border(LEFT)
+        self.play(Write(vec_b_2_tex, run_time=2))
+
+        vec_b_1_tmp = (np.vdot(self.vec_a_2, vec_b_1) / np.vdot(vec_b_1, vec_b_1)) * vec_b_1
+        vec_b_1_tmp_arrow = arrow_of(vec_b_1_tmp)
+        self.play(
+            Create(vec_b_1_tmp_arrow, run_time=2)
+        )
+
+        vec_b_2 = self.vec_a_2 - vec_b_1_tmp
+        vec_b_2_arrow_old = arrow_of(vec_b_2)
+        self.play(
+            Create(vec_b_2_arrow_old, run_time=2)
+        )
+        self.wait(1)
+
+        self.play(
+            Uncreate(vec_b_1_tmp_arrow, run_time=2)
+        )
+        vec_b_2_arrow = arrow_of(vec_b_2)
+        self.play(Transform(vec_b_2_arrow_old, vec_b_2_arrow, replace_mobject_with_target_in_scene=True, run_time=2))
         self.wait(1)
